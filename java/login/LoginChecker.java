75a36dbe1d79d1e635034201d6684930729c4933
Saman Shrestha <samanshrestha1000@gmail.com>
2022-08-02 22:20:33 +0530

    first commit

---
+++ java/login/LoginChecker.java
@@ -0,0 +1,57 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginChecker")
+public class LoginChecker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		if(uname.equalsIgnoreCase("Saman") && pword.equals("saman07")){
+			out.println("You are Welcomed");
+		}
+		else
+			out.println("Invalid Username or Password");
+		
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		//RequestDispatcher- an interface to include content on same page or different pages
+		RequestDispatcher rd;
+		if(uname.equalsIgnoreCase("Saman") && pword.equals("saman07")){
+			//session object is created bby web conatiner and generate sessionid
+			HttpSession session=request.getSession(true);//create a new object and store its reference in session object
+			session.setAttribute("username", uname);
+			
+			rd=request.getRequestDispatcher("Welcome");
+			rd.forward(request, response);
+		}
+		else {
+			out.println("Invalid Username or Password");
+			rd=request.getRequestDispatcher("LoginForm.html");
+			rd.include(request, response);
+		}
+	}
+}
